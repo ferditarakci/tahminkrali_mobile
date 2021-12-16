e616607b38acba128375d05ba2cae7c86a76f0c2
Ferdi Tarakcı <bilgi@ferditarakci.com>
2021-12-17 00:03:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tahmin Kralı - Mobile
+
+\#2016-10-04
+
+<br><img alt="" src="assets/images/screenshot_1.jpg">
+
+<br><img alt="" src="assets/images/screenshot_2.jpg">
---
+++ assets/js/common.js
@@ -0,0 +1,42 @@
+﻿
+/*
+	TAHMIN KRALI - MOBILE - 04/10/2016
+	Front-End Developer: Ferdi Tarakci
+	Web Site: ferditarakci.com
+*/
+
+jQuery(function( $ ) {
+
+	$('body > *').filter(function() {
+			var a = $(this).css('position');
+		if (a == 'relative' || a == 'fixed' || a == 'absolute') {
+			$(this).wrapInner('<div class="fixed-fixes" />')
+		}
+	});
+
+	$('a[rel~=external]').each(function() {
+		$(this).attr('target', '_blank');
+	});
+
+	var clickEvent = 'click';
+	var downEvent  = 'mousedown';
+	if (navigator.userAgent.match(/(iP(ad|od|hone))/i)) {
+		clickEvent = 'touchstart click';
+		downEvent  = 'touchstart mousedown';
+	}
+
+	$('body').wrapInner('<div id="site-wrap" />');
+	$('body').wrapInner('<div id="inner-wrap" />');
+	$('body').wrapInner('<div id="outer-wrap" />');
+
+
+	$(document).on(clickEvent, 'a[href=#]', false);
+
+
+}); /* jQuery Ready */
+
+
+
+
+
+
